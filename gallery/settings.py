97c02dda25df2cb90b23d56ac88b8d803743a836
Benson <bensongathu23@gmail.com>
2021-07-01 16:03:30 +0300

    navbar added

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'images.apps.ImagesConfig'
+    'images.apps.ImagesConfig',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
