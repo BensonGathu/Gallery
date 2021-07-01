52153706b4f3a758c0fd54368d362411835da059
Benson <bensongathu23@gmail.com>
2021-07-01 20:18:41 +0300

    Models added

--- images/models.py
+++ images/models.py
@@ -1,3 +1,66 @@
 from django.db import models
-
+import datetime as dt
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+    
+    # @classmethod
+    # def update_location(cls,location):
+    #     cls.objects.filter(location.id = location.id).update(name=)
+    #     Editor.objects.filter(id = 2).update(first_name ='Kim')
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/',default="Image")
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    loc =  models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_image_by_id(cls,image_id):
+        image = cls.objects.get(id=image_id)
+        return image
+
+    @classmethod
+    def search_image(cls,category):
+        images = cls.objects.filter(category=category).all()
+        return images
+    
+    @classmethod
+    def filter_by_location(cls,location):
+        images = Image.objects.filter(location=location).all()
+        return images
