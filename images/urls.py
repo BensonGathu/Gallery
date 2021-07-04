224a9407e7d315ce3e5322d239e081686390a067
Benson <bensongathu23@gmail.com>
2021-07-04 16:58:48 +0300

    thumbnails added

--- images/urls.py
+++ images/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r"^$",views.welcome,name="index"),
-    url(r"^search/$",views.search,name="search_results"),
-    url(r"^image/(\d+)",views.image,name="image")
+    url(r'^$',views.welcome,name='index'),
+    url(r'^search/',views.search,name='search_results'),
+    url(r'^images/(\d+)',views.image,name='image')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
